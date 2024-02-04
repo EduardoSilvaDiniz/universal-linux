cee871e58d6c5314925d3919a42d1f6574675e7a
Eduardo da Silva Diniz <eduardofox989@protonmail.com>
2024-02-03 23:18:47 -0300

    autocommit (script update-repo) date: sáb 03 fev 2024 23:18:47 -03

--- scripts/update-repos.sh
+++ scripts/update-repos.sh
@@ -4,6 +4,6 @@ dirs=$(ls ~/Git/)
 dateCurrent=$(date)
 
 for a in ${dirs[@]}; do 
-	cd ~/Git/$a && git add . && git commit -m "script update-repo date: $dateCurrent" && git push || \
+	cd ~/Git/$a && git add . && git commit -m "autocommit (script update-repo) date: $dateCurrent" && git push || \
 		echo "error git $a" >> ~/update-repo.log
 done
