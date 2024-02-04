6b8d4c0c6b9e5157c6c59269e666c95f095eee8b
Eduardo da Silva Diniz <eduardofox989@protonmail.com>
2024-02-03 23:25:31 -0300

    autocommit (script update-repo) date: sáb 03 fev 2024 23:25:31 -03

--- scripts/update-repos.sh
+++ scripts/update-repos.sh
@@ -2,8 +2,13 @@
 
 dirs=$(ls ~/Git/)
 dateCurrent=$(date)
+#TODO adiciona tratamento de erros
 
 for a in ${dirs[@]}; do 
 	cd ~/Git/$a && git add . && git commit -m "autocommit (script update-repo) date: $dateCurrent" && git push || \
-		echo "error git $a" >> ~/update-repo.log
+	echo "error git $a" >> ~/update-repo.log
 done
+mineDir="/home/edu/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/Better_than_Adventure_7.1_Prerelease_1a (1)/.minecraft/saves/BTA-save"
+
+cd ~/$mineDir/Better_than_Adventure_7.1_Prerelease_1a (1)/.minecraft/saves/BTA-save && git add . && git commit -m "autocommit (script update-repo) date: $dateCurrent" && git push || \
+	echo "error git $a" >> ~/update-repo.log
